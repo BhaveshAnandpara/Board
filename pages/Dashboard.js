ff7d8c9959167994951ee956347c807160d6154c
Bhavesh Anandpara <bhaveshanandpara12@gmail.com>
2023-09-03 14:35:09 +0530

    activity block done

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -27,7 +27,7 @@ export default function Dashboard() {
 
                             <div className="inp bg-[#fff] mr-[14px] flex justify-center items-center flex-row px-[15px] py-[7px] rounded-[10px] ">
                                 <input type="text" name=" " id="" placeholder="Search..." />
-                                  <Image src={search} width={12} height={12} />  
+                                <Image src={search} width={12} height={12} />
 
                             </div>
 
@@ -41,9 +41,9 @@ export default function Dashboard() {
 
                     </div>
 
-                    <div className="cards flex flex-row gap-[17px] w-full">
+                    <div className="cards flex flex-row gap-[17px] w-full ">
 
-                        <div className="card flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
+                        <div className="card max-w-[230px] flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
 
                             <div className="icon bg-[#7FCD93] w-[30px] h-[30px] flex justify-center items-center rounded-full mb-[8px]">
                                 <Image src={RevenueIcon} height={18} width={18} className=" translate-y-[-2px]" />
@@ -61,10 +61,10 @@ export default function Dashboard() {
 
                         </div>
 
-                        <div className="card flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
+                        <div className="card max-w-[230px] flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
 
                             <div className="icon bg-[#DEBF85] w-[30px] h-[30px] flex justify-center items-center rounded-full mb-[8px]">
-                                <Image src={TransactionIcon} height={18} width={18}  />
+                                <Image src={TransactionIcon} height={18} width={18} />
                             </div>
 
                             <p className=" font-secondary text-[11px] text-[#000]" >Total Transactions</p>
@@ -79,10 +79,10 @@ export default function Dashboard() {
 
                         </div>
 
-                        <div className="card flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
+                        <div className="card max-w-[230px] flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
 
                             <div className="icon bg-[#ECA4A4] w-[30px] h-[30px] flex justify-center items-center rounded-full mb-[8px]">
-                                <Image src={LikesIcon} height={18} width={18}  />
+                                <Image src={LikesIcon} height={18} width={18} />
                             </div>
 
                             <p className=" font-secondary text-[11px] text-[#000]" >Total Likes</p>
@@ -97,10 +97,10 @@ export default function Dashboard() {
 
                         </div>
 
-                        <div className="card flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
+                        <div className="card max-w-[230px] flex-1 py-[18px] px-[26px] shadow border-solid border-[#e0e0e0] border-2 bg-[#fff] rounded-[20px]">
 
                             <div className="icon bg-[#A9B0E5] w-[30px] h-[30px] flex justify-center items-center rounded-full mb-[8px]">
-                                <Image src={UserIcon} height={18} width={18}  />
+                                <Image src={UserIcon} height={18} width={18} />
                             </div>
 
                             <p className=" font-secondary text-[11px] text-[#000]" >Total Users</p>
@@ -114,6 +114,133 @@ export default function Dashboard() {
                             </div>
 
                         </div>
+
+                    </div>
+
+                    <div className="act-card w-full bg-[#fff] rounded-[10px] py-[30px] px-[40px]">
+
+                        <div className=" overflow-hidden">
+
+                            <div className="header flex flex-row justify-between items-center">
+
+                                <div  >
+                                    <p className=" font-primary text-[18px] font-bold" >
+                                        Activities
+                                    </p>
+                                    <p className=" font-primary text-[14px] font-normal text-[#858585]" >May - June 2021</p>
+
+                                </div>
+
+                                <div className="legened flex flex-row justify-center items-center">
+
+                                    <div className="dot w-[10px] h-[10px] mr-[10px] bg-[#E9A0A0] rounded-full">  </div>
+                                    <p className=" font-secondary text-[14px] mr-[35px]"> Guest </p>
+
+                                    <div className="dot w-[10px] h-[10px] mr-[10px] bg-[#9BDD7C] rounded-full">  </div>
+                                    <p className=" font-secondary text-[14px] mr-[35px]"> User </p>
+
+                                </div>
+
+                            </div>
+
+                            <div className="graphs absolute w-[70%] z-10 mt-[30px] flex flex-row items-baseline justify-center gap-[110px] pl-[14px]">
+
+                                <div className="graph1 flex flex-col items-center text-[#858585] font-secondary text-[14px] ">
+
+                                    <div className="flex flex-row gap-[10px] items-baseline" >
+
+                                        <div className="bar1 rounded-[4px] bg-[#98D89E] h-[200px] w-[38px] "></div>
+                                        <div className="bar2 rounded-[4px] bg-[#EE8484] h-[80px] w-[38px] "></div>
+
+                                    </div>
+
+                                    <p>Week 1</p>
+                                </div>
+
+                                <div className="graph2 flex flex-col items-center text-[#858585] font-secondary text-[14px] ">
+
+                                    <div className="flex flex-row gap-[10px] items-baseline" >
+
+                                        <div className="bar1 rounded-[4px] bg-[#98D89E] h-[152px] w-[38px] "></div>
+                                        <div className="bar2 rounded-[4px] bg-[#EE8484] h-[168px] w-[38px] "></div>
+
+                                    </div>
+
+                                    <p>Week 2</p>
+                                </div>
+
+                                <div className="graph3 flex flex-col items-center text-[#858585] font-secondary text-[14px] ">
+
+                                    <div className="flex flex-row gap-[10px] items-baseline" >
+
+                                        <div className="bar1 rounded-[4px] bg-[#98D89E] h-[80px] w-[38px] "></div>
+                                        <div className="bar2 rounded-[4px] bg-[#EE8484] h-[120px] w-[38px] "></div>
+
+                                    </div>
+
+                                    <p>Week 3</p>
+                                </div>
+
+                                <div className="graph4 flex flex-col items-center text-[#858585] font-secondary text-[14px] ">
+
+                                    <div className="flex flex-row gap-[10px] items-baseline" >
+
+                                        <div className="bar1 rounded-[4px] bg-[#98D89E] h-[160px] w-[38px] "></div>
+                                        <div className="bar2 rounded-[4px] bg-[#EE8484] h-[128px] w-[38px] "></div>
+
+                                    </div>
+
+                                    <p>Week 4</p>
+                                </div>
+
+                            </div>
+
+                            <div className="chart flex flex-col  translate-x-[-20px]">
+
+                                <div className="line flex flex-row justify-center items-center text-[#858585] mt-[16px]">
+
+                                    <p className="mr-[15px] tezt-[14px]" >500</p>
+                                    <hr className="w-[90%] text-[#EAEAEA]" />
+
+                                </div>
+                                <div className="line flex flex-row justify-center items-center text-[#858585] mt-[16px]">
+
+                                    <p className="mr-[15px] tezt-[14px]" >400</p>
+                                    <hr className="w-[90%] text-[#EAEAEA]" />
+
+                                </div>
+                                <div className="line flex flex-row justify-center items-center text-[#858585] mt-[16px]">
+
+                                    <p className="mr-[15px] tezt-[14px]" >300</p>
+                                    <hr className="w-[90%] text-[#EAEAEA]" />
+
+                                </div>
+                                <div className="line flex flex-row justify-center items-center text-[#858585] mt-[16px]">
+
+                                    <p className="mr-[15px] tezt-[14px]" >200</p>
+                                    <hr className="w-[90%] text-[#EAEAEA]" />
+
+                                </div>
+                                <div className="line flex flex-row justify-center items-center text-[#858585] mt-[16px]">
+
+                                    <p className="mr-[15px] tezt-[14px]" >100</p>
+                                    <hr className="w-[90%] text-[#EAEAEA]" />
+
+                                </div>
+
+                                <div className="line flex flex-row justify-center items-center text-[#858585] mt-[16px]">
+
+                                    <p className="mr-[15px] tezt-[14px]" >0</p>
+                                    <hr className="w-[90%] text-[#EAEAEA]" />
+
+                                </div>
+
+                            </div>
+
+
+
+                        </div>
+
 
                     </div>
 
